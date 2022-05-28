cbb657dbab777a31f80319148c05aecd494a3664
U631529 <nadiaz2@teco.com.ar>
2022-05-28 01:19:58 -0300

    albumList

--- components/AlbumList.js
+++ components/AlbumList.js
@@ -1,45 +1,45 @@
-import React, {Component} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, StyleSheet, Text, View, StatusBar} from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
-class AlbumList extends Component {
-  state = {photoset: null};
 
-  componentWillMount() {
-    axios
-      .get(
-        'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1',
-      )
-      .then((response) =>
-        this.setState({photoset: response.data.photosets.photoset}),
-      );
+const AlbumList = ({navigation}) => {
+  const [state, setState] = useState({ photoset: null })
+
+  const componentWillMount = async () =>  {
+    const response = await axios.get(
+      'https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=6e8a597cb502b7b95dbd46a46e25db8d&user_id=137290658%40N08&format=json&nojsoncallback=1',
+    )
+      setState({photoset: response.data.photosets.photoset});
+   
   }
-
-  renderAlbums() {
-    return this.state.photoset.map((album) => (
-      <AlbumDetail
-        navigation={this.props.navigation}
-        key={album.id}
-        title={album.title._content}
-        albumId={album.id}
+  
+  useEffect(() => {
+    componentWillMount()
+  }, [])
+  
+  const renderAlbums = ({item}) => {
+      return <AlbumDetail
+        navigation={navigation}
+        key={item.id}
+        title={item.title._content}
+        albumId={item.id}
       />
-    ));
+    
   }
 
-  render() {
-    console.log(this.state);
-
-    if (!this.state.photoset) {
-      return <Text>Loading...</Text>;
-    }
-
-    return (
-      <View style={{flex: 1}}>
-        <ScrollView>{this.renderAlbums()}</ScrollView>
-      </View>
-    );
-  }
+  return (
+    state.photoset ?
+      <View>
+        <FlatList
+          data={state.photoset}
+          renderItem={renderAlbums}
+          keyExtractor={album => album.id}
+        />
+      </View> :
+      <Text>Loading...</Text>
+  )
 }
 
 export default AlbumList;
---
+++ index.js
@@ -0,0 +1,5 @@
+import {AppRegistry} from 'react-native';
+import {expo as appName} from './app.json';
+import { App } from './app.js';
+
+AppRegistry.registerComponent(appName, () => App);
